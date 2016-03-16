411a411fdee8229bcf08c9fdfc235772d87656bd
John <morman@outlook.com>
2016-03-16 17:34:22 +0100

    Fix casing issue. And now delete is ready but has to be uncommented to run.

--- delete.rb
+++ delete.rb
@@ -1,4 +1,5 @@
 require 'net/http'
+require 'fileutils'
 require 'uri'
 require 'json'
 
@@ -18,11 +19,10 @@ end
 
 def getFilesInDir
 	showsOnDisk = Hash.new
-	# Location where files are stored
 	Dir.entries('../torrents/data/tv').each do |x|
 		tempMatch = /^(?<showname>.*)[\.|\s]+S(?<season>\d{2})E(?<episode>\d{2})/.match(x)
 		if tempMatch != nil then
-			showname = tempMatch["showname"].gsub(/\./, " ")
+			showname = tempMatch["showname"].gsub(/\./, " ").downcase
 			season = tempMatch["season"].to_i
 			episode = tempMatch["episode"].to_i
 			if showsOnDisk[showname] == nil then
@@ -49,25 +49,40 @@ watched = getWatchedTvShows()
 json = JSON.parse(watched)
 loc = 0
 json.each do |x|	
-	showsOnTrakt[x["show"]["title"].gsub(/\'/, "")] = loc;
+	showsOnTrakt[x["show"]["title"].gsub(/\'/, "").downcase]= loc;
 	loc = loc + 1
 end
 
 
+
 showsOnDisk.keys.each do |showname|
+	#puts showname
 	showsOnDisk[showname].keys.each do |season|
+		# Check to ensure that the show exists on trakt
+		if showsOnTrakt[showname] == nil then
+			next
+		end
 		# Check to see if the season exists in the array. If it doesn't exist then it means
 		# that we haven't watched one episod ein the season yet.
 		if json[showsOnTrakt[showname]]["seasons"][season - 1] == nil then
 			next
 		end
+
 		showsOnDisk[showname][season].keys.each do |episode|
 			# Check to see if the episode exists in the array. If it doesn't exist then it means
 			# that the episode hasn't been watched yet.
+			json[showsOnTrakt[showname]]["seasons"][season-1]["episodes"].each do |traktEpisode|
+				#puts traktEpisode["number"]
+				if traktEpisode["number"] == episode  then
+					puts showname + " " + season.to_s + " " + episode.to_s + "        " + showsOnDisk[showname][season][episode]
+					#FileUtils.rm_rf('../torrents/data/tv/' + showsOnDisk[showname][season][episode])
+				end	
+			end
+				
 			if json[showsOnTrakt[showname]]["seasons"][season - 1]["episodes"][episode - 1] == nil then
 				next
 			end
-				puts showname + " " + season.to_s + " " + episode.to_s + "        " + showsOnDisk[showname][season][episode]
+
 		end
 	end
 end
